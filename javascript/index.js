3664f8e353f9ef781ec034e6318d2cfc39730199
Marcus Neto <marcusnetoo@outlook.com>
2024-10-31 20:07:44 -0300

    update

---
+++ javascript/index.js
@@ -0,0 +1,25 @@
+function redirectToPage() {
+    window.open("programacao1.html", "_blank"); 
+}
+
+function redirectToPage2() {
+    window.open("algoritmos2.html", "_blank"); 
+}
+
+/*
+function redirectToPage3() {
+    window.open("senhas.html", "_blank"); 
+}
+
+function redirectToPage4() {
+    window.open("links.html", "_blank"); 
+}
+
+function increaseSize(element) {
+    element.classList.add("aumentar");
+}
+
+function resetSize(element) {
+    element.classList.remove("aumentar");
+}
+    */

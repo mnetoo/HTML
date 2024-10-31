544478fec55fd04a447a2d175943c32c8c93471c
Marcus Neto <marcusnetoo@outlook.com>
2024-10-31 20:12:43 -0300

    ajustes

--- javascript/index.js
+++ javascript/index.js
@@ -1,25 +1,7 @@
 function redirectToPage() {
-    window.open("programacao1.html", "_blank"); 
+    window.open("pages/programacao1.html", "_blank"); 
 }
 
 function redirectToPage2() {
-    window.open("algoritmos2.html", "_blank"); 
+    window.open("pages/algoritmos2.html", "_blank"); 
 }
-
-/*
-function redirectToPage3() {
-    window.open("senhas.html", "_blank"); 
-}
-
-function redirectToPage4() {
-    window.open("links.html", "_blank"); 
-}
-
-function increaseSize(element) {
-    element.classList.add("aumentar");
-}
-
-function resetSize(element) {
-    element.classList.remove("aumentar");
-}
-    */
